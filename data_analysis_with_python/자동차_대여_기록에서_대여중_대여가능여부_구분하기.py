ec35dcbe38f22e5ba368c0dd11facfd3a4e7117a
Sejung Son <aaissj17@gmail.com>
2023-09-21 23:52:48 +0900

    Create 자동차_대여_기록에서_대여중_대여가능여부_구분하기.py

---
+++ data_analysis_with_python/자동차_대여_기록에서_대여중_대여가능여부_구분하기.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/157340
+import pandas as pd
+
+
+df = pd.read_csv('/content/drive/MyDrive/코딩테스트/data/data6.csv')
+df['START_DATE'] = pd.to_datetime(df['START_DATE'])
+df['END_DATE'] = pd.to_datetime(df['END_DATE'])
+
+df['AVAILABILITY'] = df.apply(lambda x: "대여중" if x['START_DATE']<= datetime.datetime.strptime('2022-10-16',"%Y-%m-%d") <=x['END_DATE'] else "대여 가능", axis=1)
+result_df = df.groupby('CAR_ID')['AVAILABILITY'].apply(lambda x: '대여중' if '대여중' in x.values else '대여 가능').reset_index()
+
