b9802e30e4bc47cf18b85f98c0fbd78c5464ff8e
Sejung Son <aaissj17@gmail.com>
2024-01-16 15:06:37 +0900

    Update 2798.py

--- 2024/Brute-force/2798.py
+++ 2024/Brute-force/2798.py
@@ -1,4 +1,4 @@
-N, M = map(int, input().split())
+N, M = map(int, input().split()) # 첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000) -> Brute-force로 풀어도 최대 10^6 < 10^8
 arr = list(map(int, input().split()))
 
 max_num = 0 
