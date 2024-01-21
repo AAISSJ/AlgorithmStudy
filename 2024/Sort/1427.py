1d13492a3f3140c1f9a5ab7d16073d7b44925a47
Sejung Son <aaissj17@gmail.com>
2024-01-21 17:03:18 +0900

    Create 1427.py

---
+++ 2024/Sort/1427.py
@@ -0,0 +1,16 @@
+import sys 
+input = sys.stdin.readline 
+
+
+N = int(input())
+str_N = str(N)
+
+tmp = [0] * 10
+for i in str_N:
+  tmp[int(i)]+=1
+
+
+for i in range(9, -1, -1):
+  if tmp[i]!=0: 
+    for j in range(tmp[i]):
+      print(i,end='')
