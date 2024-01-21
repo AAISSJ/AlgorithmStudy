ff5cc87d7d89495a1605d35f7d7eaff9e56007f8
Sejung Son <aaissj17@gmail.com>
2024-01-21 18:18:23 +0900

    Create 18870.py

---
+++ 2024/Sort/18870.py
@@ -0,0 +1,23 @@
+import sys 
+input = sys.stdin.readline
+
+N = int(input())
+tmp = list(map(int, input().split()))
+
+sort_tmp = sorted(set(tmp))  # nlogn
+
+dic ={}
+for index, num in enumerate(sort_tmp): # n 
+  dic[num] = index
+
+for i in tmp: # n 
+  print(dic[i], end=' ')
+
+# 시간 복잡도 크게 짠 경우
+# N = int(input())
+# tmp = list(map(int, input().split()))
+
+# sort_tmp = sorted(set(tmp))  # nlogn
+
+# for i in tmp:  # N^2
+#   print(sort_tmp.index(i),end=' ')
