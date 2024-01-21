190b8c1943a6abd6e72fabad039a9887059ec8bf
Sejung Son <aaissj17@gmail.com>
2024-01-21 17:06:52 +0900

    Update 1427.py

--- 2024/Sort/1427.py
+++ 2024/Sort/1427.py
@@ -1,21 +1,22 @@
 import sys 
 input = sys.stdin.readline 
 
+N = list(input())
+N.sort(reverse=True)
+print(''.join(N))
 
-N = int(input())
-str_N = str(N)
+# N = int(input())
+# str_N = str(N)
 
-tmp = [0] * 10
-for i in str_N:
-  tmp[int(i)]+=1
-
-
-for i in range(9, -1, -1):
-  if tmp[i]!=0: 
-    for j in range(tmp[i]):
-      print(i,end='')
+# tmp = [0] * 10
+# for i in str_N:
+#   tmp[int(i)]+=1
 
 
-# N = list(input())
-# N.sort(reverse=True)
-# print(''.join(N))
+# for i in range(9, -1, -1):
+#   if tmp[i]!=0: 
+#     for j in range(tmp[i]):
+#       print(i,end='')
+
+
+
