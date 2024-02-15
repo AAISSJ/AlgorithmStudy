24440c4e02e19099f6dea9c203628f9ba44ff331
Sejung Son <aaissj17@gmail.com>
2024-02-15 09:12:16 +0900

    Create 11279.py

---
+++ 2024/Data Structure/Priority Queue/11279.py
@@ -0,0 +1,19 @@
+import heapq 
+import sys 
+
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = []
+heapq.heapify(arr)
+
+for _ in range(N):
+    num = int(input())
+    if num == 0:
+        if arr :
+            print(-heapq.heappop(arr))
+        else : 
+            print(0)
+    else :
+        heapq.heappush(arr,-num)
