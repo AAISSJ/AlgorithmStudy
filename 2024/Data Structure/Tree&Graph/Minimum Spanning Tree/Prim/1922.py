e517408573a49327a3fbccfbf96653063a4d482c
Sejung Son <aaissj17@gmail.com>
2024-02-21 13:16:37 +0900

    Update 1922.py

--- 2024/Data Structure/Tree&Graph/Minimum Spanning Tree/Prim/1922.py
+++ 2024/Data Structure/Tree&Graph/Minimum Spanning Tree/Prim/1922.py
@@ -1,34 +1,43 @@
+# 연결하는 비용 최소 -> 최단 경로 or 최소 신장 트리 
+    # 최단 경로 : 가중 그래프 상의 두 노드를 연결하는 가장 짧은 경로를 찾는 방법 / 방향&무방향
+    # 최소 신장 트리 : 그래프 상의 모든 노드들을 최소 비용으로 연결하는 방법 / 무방향 only
+# 본 문제에서는 모든 컴퓨터를 연결하는 데 필요한 최소 비용이므로 최소 신장 트리 
+    # 크루스칼 또는 프림 알고리즘을 이용하면 되겠다 
+
 from heapq import *
-import sys
-input = sys.stdin.readline
-
-def prim2(s):
-    INF = 10001
-    visited = [0] * (N + 1)     # MST에 포함된 노드들, 인덱스를 위해 (N+1)개
-    weight = [INF] * (N + 1)    # 각 노드별 MST에 포함될 때의 소비한 가중치, 인덱스를 윌해 (N+1)개
-
-    weight[s] = 0               # 시작노드는 가중치 0
-    edges_heap = [(0, s)]       # 시작노드에 대한 정보를 heap에 넣어줘야 함 (가중치, 노드)
-    heapify(edges_heap)         # heapify함수로 원래 list였던 edges_heap을 자료구조 heap으로 변경시켜줌
-
-    while edges_heap:           # 힙이 소진될 때까지 반복
-        mn, i_min = heappop(edges_heap) # python의 heapq는 최소힙으로, 항상 최소 가중치의 노드가 나옴
-        if not visited[i_min]:          # 아직 MST에 포함되지 않은 인접 노드일 경우, 연산진행
-            visited[i_min] = 1              # MST에 포함시킴
-            for w, adj in adjLst[i_min]:    # 새로 MST에 추가된 노드에 대해서 인접 노드 가중치 최솟값으로 업데이트
-                if not visited[adj] and weight[adj] > w:    # MST의 인접노드이며 & 최솟값일 경우 업데이트
-                    weight[adj] = w
-                    heappush(edges_heap, (w, adj))          # 해당 정보는 heap에 넣어줌 (가중치, 노드)
-
-    print(sum(weight[1:]))
-
+ 
 N = int(input())
 M = int(input())
 
-adjLst = [[] for _ in range(N + 1)]
-for _ in range(M):
-    i, j, w = map(int, input().split())
-    adjLst[i].append((w, j))
-    adjLst[j].append((w, i))
 
-prim2(1)
+# 1. 그래프 초기화 like DFS 
+graph = {i:[] for i in range(1,N+1)}
+visited = [0 for i in range(N+1)]
+weight = [10001 for i in range(N+1)]
+
+for _ in range(M):
+    a,b,c = map(int, input().split())
+    graph[a].append((c,b))
+    graph[b].append((c,a))
+
+
+def prim(node):
+    # 처음 시작 노드 PQ 에 넣어주기 & 가중치는 0 
+    weight[node] = 0 
+    pq = [(0, node)]
+    heapify(pq) 
+    
+    # 아래로 내려가기 
+    while pq: 
+        now_w, now_n = heappop(pq)
+        if not visited[now_n]:
+            visited[now_n]=1
+            for next_w, next_n in graph[now_n]:
+                if not visited[next_n] and weight[next_n]>next_w:
+                    weight[next_n]=next_w
+                    heappush(pq,(next_w, next_n))
+
+prim(1) # 1부터 시작 - 그냥 임의로 
+print(sum(weight[1:]))
+    
+    
