6d3e4b01f2eb51fb2ee7ecbc587749c62272ff5d
Sejung Son <aaissj17@gmail.com>
2024-02-23 12:33:52 +0900

    Update 15649.py

--- 2024/Exhaustive Search/Backtracking/15649.py
+++ 2024/Exhaustive Search/Backtracking/15649.py
@@ -2,6 +2,9 @@
 # - Promising : 해당 루트가 조건에 맞는지 검사 & Pruning : 조간에 맞지 않으면 자른다 
 # - dfs (재귀) 기반으로 풀되, Pruning한다는 점이 다름 
 
+import sys 
+input = sys.stdin.readline
+
 N,M = map(int, input().split())
 arr = [] 
 
