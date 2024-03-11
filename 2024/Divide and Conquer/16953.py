7bc3ba5a822bfe1e42bcadf2e534895394aaeadf
Sejung Son <aaissj17@gmail.com>
2024-03-11 11:27:03 +0900

    Update 16953.py

--- 2024/Divide and Conquer/16953.py
+++ 2024/Divide and Conquer/16953.py
@@ -30,3 +30,63 @@ if cnt==-1:
     print(-1)
 else: 
     print(cnt+1)
+
+# 다른 풀이 1. while문으로 푼 거 
+#
+# import sys
+# input=sys.stdin.readline
+# a,b=map(int,input().split())
+# count=1
+# while True:
+#     if b==a:
+#         break
+#     elif (b%2!=0 and b%10!=1)or (b<a):
+#         count=-1
+#         break
+#     else:
+#         if b%10==1:
+#             b//=10
+#             count+=1
+#         else:
+#             b//=2
+#             count+=1
+# print(count)
+
+# 다른 풀이 2. bfs로 푼 사람 
+# 
+# import sys
+# import collections
+
+# input = sys.stdin.readline
+
+# a, b = map(int, input().split())
+# queue = collections.deque()
+
+# queue.append(a)
+
+
+# def bfs():
+#     cnt = 1
+#     while queue:
+#         size = len(queue)
+#         for _ in range(size):
+#             cur = queue.popleft()
+#             if b < cur:
+#                 continue
+#             if cur == b:
+#                 queue.append(a)
+#                 print(cnt)
+#                 return
+#             else:
+#                 queue.append(cur * 2)
+#                 queue.append(cur * 10 + 1)
+#         cnt += 1
+
+
+# bfs()
+# if not queue:
+#     print(-1)
+
+
+
+    
