f7bf36e202f16a697bfd3b30bee92ee946d6ca5c
Sejung Son <aaissj17@gmail.com>
2024-03-21 22:19:29 +0900

    Update 11727.py

--- 2024/DP/11727.py
+++ 2024/DP/11727.py
@@ -3,14 +3,15 @@ import sys
 input =sys.stdin.readline
 
 N = int(input())
-DP = [ 0 for i in range(N+1)]
+DP = [0 for i in range(N+1)]
 
 
-DP[1]=1
-DP[2]=3
-
-for i in range(3,N+1):
-    DP[i] = DP[i-1]+2*DP[i-2]
+for i in range(1,N+1):
+    if i==1:
+        DP[i]=1
+    elif i==2:
+        DP[i]=3
+    else:
+        DP[i] = DP[i-1]+2*DP[i-2]
     
-
 print(DP[N]%10007)
